6fd7ebf1441dce522df2bdb7364663c4cff8d44e
doct0rX <mustafaj4m@gmail.com>
2018-02-14 19:56:33 +0200

    lesson18

---
+++ FullStack/lessons/Turtle/trtle.py
@@ -0,0 +1,16 @@
+import turtle 
+
+painter = turtle.Turtle()
+
+painter.pencolor("blue")
+
+for i in range(50):
+    painter.forward(50)
+    painter.left(123) # Let's go counterclockwise this time 
+    
+painter.pencolor("red")
+for i in range(50):
+    painter.forward(100)
+    painter.left(123)
+    
+turtle.done()
