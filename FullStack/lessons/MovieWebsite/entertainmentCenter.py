2f738f8d57c22f59acb4efb3ca47f1b14bbe0d90
doct0rX <mustafaj4m@gmail.com>
2018-02-15 02:47:29 +0200

    lesson 21

--- FullStack/lessons/MovieWebsite/entertainmentCenter.py
+++ FullStack/lessons/MovieWebsite/entertainmentCenter.py
@@ -1,3 +1,10 @@
 import media
 
-toyStory = media.Movie()
+decipherX = media.Movie("decipherX",
+                        "an application to remove watermark",
+                        "https://www.google.com.eg/search?q=watermark+images&tbm=isch&source=iu&ictx=1&fir=szl4_8TCrWOjZM%253A%252C94acLUgkrsmMRM%252C_&usg=__Suw3mAzRaC0D0MRhiTByI3XQosk%3D&sa=X&ved=0ahUKEwipxNe-06bZAhXF6RQKHS8PCkwQ9QEIJzAA#imgrc=szl4_8TCrWOjZM:",
+                        "https://www.youtube.com/watch?v=XomH8rBJfUk")
+
+# print(toyStory.storyline)
+# toyStory.showTrailer()
+
--- FullStack/lessons/MovieWebsite/media.py
+++ FullStack/lessons/MovieWebsite/media.py
@@ -1,5 +1,10 @@
+import webbrowser
 class Movie():
     def __init__(self, title, storyline, posterImage, trailer):
         self.title = title
         self.storyline = storyline
+        self.posterImage = posterImage
+        self.trailer = trailer
 
+    def showTrailer(self):
+        webbrowser.open(self.trailer)
