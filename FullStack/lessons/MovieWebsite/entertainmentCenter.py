b1bb3c6e8f8b96256270a6026d5c8cefad021d1f
doct0rX <mustafaj4m@gmail.com>
2018-02-15 02:17:46 +0200

    lesson21

---
+++ FullStack/lessons/MovieWebsite/entertainmentCenter.py
@@ -0,0 +1,3 @@
+import media
+
+toyStory = media.Movie()
---
+++ FullStack/lessons/MovieWebsite/media.py
@@ -0,0 +1,5 @@
+class Movie():
+    def __init__(self, title, storyline, posterImage, trailer):
+        self.title = title
+        self.storyline = storyline
+
