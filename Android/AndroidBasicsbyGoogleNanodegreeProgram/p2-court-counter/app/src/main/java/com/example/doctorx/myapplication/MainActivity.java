74a570811efd980f4c0f300970f5edcc0bd01192
doct0rX <mustafaj4m@gmail.com>
2018-02-25 23:40:39 +0200

    add win to teams --complete functionality--

--- Android/AndroidBasicsbyGoogleNanodegreeProgram/p2-court-counter/app/src/main/java/com/example/doctorx/myapplication/MainActivity.java
+++ Android/AndroidBasicsbyGoogleNanodegreeProgram/p2-court-counter/app/src/main/java/com/example/doctorx/myapplication/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         zGoals += 1;
         TextView textGoal = findViewById(R.id.z_goals);
         textGoal.setText(String.valueOf(zGoals));
+        teamWin(v);
     }
 
     public void setzFouls(View v) {
@@ -34,11 +35,27 @@ public class MainActivity extends AppCompatActivity {
         aGoals += 1;
         TextView textGoal = findViewById(R.id.a_goals);
         textGoal.setText(String.valueOf(aGoals));
+        teamWin(v);
     }
 
     public void setaFouls(View v) {
         aFouls += 1;
         TextView textFoul = findViewById(R.id.a_fouls);
         textFoul.setText(String.valueOf(aFouls));
+    }
+
+    public void teamWin(View v) {
+        if (zGoals > aGoals) {
+            TextView textWin = findViewById(R.id.z_win);
+            textWin.setText(R.string.win);
+        } else if (zGoals < aGoals) {
+            TextView textWin = findViewById(R.id.a_win);
+            textWin.setText(R.string.win);
+        } else {
+            TextView textWin1 = findViewById(R.id.a_win);
+            TextView textWin2 = findViewById(R.id.z_win);
+            textWin1.setText("--");
+            textWin2.setText("--");
+        }
     }
 }
