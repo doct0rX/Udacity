6aafc7b7ef88894171dc4883ee29ac1790ab9bdc
doct0rX <mustafaj4m@gmail.com>
2018-02-26 00:32:13 +0200

    finished second project now it's all functional 👌

--- Android/AndroidBasicsbyGoogleNanodegreeProgram/p2-court-counter/app/src/main/java/com/example/doctorx/myapplication/MainActivity.java
+++ Android/AndroidBasicsbyGoogleNanodegreeProgram/p2-court-counter/app/src/main/java/com/example/doctorx/myapplication/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     int zFouls = 0;
     int aGoals = 0;
     int aFouls = 0;
+    TextView textGoal,textGoal2, textFoul, textWin, textWin1, textWin2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,42 +21,61 @@ public class MainActivity extends AppCompatActivity {
 
     public void setzGoals(View v) {
         zGoals += 1;
-        TextView textGoal = findViewById(R.id.z_goals);
+        textGoal = findViewById(R.id.z_goals);
         textGoal.setText(String.valueOf(zGoals));
         teamWin(v);
     }
 
     public void setzFouls(View v) {
         zFouls += 1;
-        TextView textFoul = findViewById(R.id.z_fouls);
+        textFoul = findViewById(R.id.z_fouls);
         textFoul.setText(String.valueOf(zFouls));
     }
 
     public void setaGoals(View v) {
         aGoals += 1;
-        TextView textGoal = findViewById(R.id.a_goals);
+        textGoal = findViewById(R.id.a_goals);
         textGoal.setText(String.valueOf(aGoals));
         teamWin(v);
     }
 
     public void setaFouls(View v) {
         aFouls += 1;
-        TextView textFoul = findViewById(R.id.a_fouls);
+        textFoul = findViewById(R.id.a_fouls);
         textFoul.setText(String.valueOf(aFouls));
     }
 
     public void teamWin(View v) {
         if (zGoals > aGoals) {
-            TextView textWin = findViewById(R.id.z_win);
+            textWin = findViewById(R.id.z_win);
             textWin.setText(R.string.win);
         } else if (zGoals < aGoals) {
-            TextView textWin = findViewById(R.id.a_win);
+            textWin = findViewById(R.id.a_win);
             textWin.setText(R.string.win);
         } else {
-            TextView textWin1 = findViewById(R.id.a_win);
-            TextView textWin2 = findViewById(R.id.z_win);
+            textWin1 = findViewById(R.id.a_win);
+            textWin2 = findViewById(R.id.z_win);
             textWin1.setText("--");
             textWin2.setText("--");
         }
+    }
+
+    public void reset(View v){
+        zFouls = 0;
+        aFouls = 0;
+        zGoals = 0;
+        aGoals = 0;
+        textWin1 = findViewById(R.id.a_win);
+        textWin2 = findViewById(R.id.z_win);
+        textWin1.setText("--");
+        textWin2.setText("--");
+        textGoal = findViewById(R.id.z_goals);
+        textGoal2 = findViewById(R.id.a_goals);
+        textGoal.setText(String.valueOf(aGoals));
+        textGoal.setText(String.valueOf(zGoals));
+        textFoul = findViewById(R.id.z_fouls);
+        textFoul.setText(String.valueOf(zFouls));
+        textFoul = findViewById(R.id.a_fouls);
+        textFoul.setText(String.valueOf(aFouls));
     }
 }
