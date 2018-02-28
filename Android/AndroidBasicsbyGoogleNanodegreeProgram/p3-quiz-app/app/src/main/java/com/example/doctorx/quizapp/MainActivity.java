dc779656d12208e22b1678fb0e564b6a688ae48b
doct0rX <mustafaj4m@gmail.com>
2018-02-28 16:35:39 +0200

    Code Review Issues Fixed --allDone-- * Grade logic for checkbox when choosing for multible correct and wrong answers * Add EditText Question --Alfred Binet-- * Added more descriptive IDs for CheckBoxs * total score become 60 instead of 50

--- Android/AndroidBasicsbyGoogleNanodegreeProgram/p3-quiz-app/app/src/main/java/com/example/doctorx/quizapp/MainActivity.java
+++ Android/AndroidBasicsbyGoogleNanodegreeProgram/p3-quiz-app/app/src/main/java/com/example/doctorx/quizapp/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         final RadioButton rb2 = findViewById(R.id.ans2);
         final RadioButton rb3 = findViewById(R.id.ans3);
         final RadioButton rb4 = findViewById(R.id.ans4);
+        EditText qSix = findViewById(R.id.qSix);
 
         String ans;
         if (rb.isChecked()) {
@@ -62,11 +63,11 @@ public class MainActivity extends AppCompatActivity {
 
 
         // Check Buttons
-        final CheckBox cb1 = findViewById(R.id.two);
-        final CheckBox cb2 = findViewById(R.id.eleven);
-        final CheckBox cb3 = findViewById(R.id.seven);
-        final CheckBox cb4 = findViewById(R.id.one);
-        final CheckBox cb0 = findViewById(R.id.zero);
+        final CheckBox cb1 = findViewById(R.id.cb_two);
+        final CheckBox cb2 = findViewById(R.id.cb_eleven);
+        final CheckBox cb3 = findViewById(R.id.cb_seven);
+        final CheckBox cb4 = findViewById(R.id.cb_one);
+        final CheckBox cb0 = findViewById(R.id.cb_zero);
 
         if (cb1.isChecked()) {
             score += 10/2;
@@ -79,19 +80,30 @@ public class MainActivity extends AppCompatActivity {
             ans += "\n 11 is a prime Number";
         }
 
-        if (cb1.isChecked() && cb2.isChecked()) {
-            ans += "\nYou Answered Question 5 Correctly";
+        if (cb1.isChecked() && cb2.isChecked() && !cb3.isChecked() && !cb4.isChecked() && !cb0.isChecked()) {
+            ans += getString(R.string.q_five_correct);
         } else if (cb3.isChecked() || cb4.isChecked() || cb0.isChecked()){
-            ans += "\nYou have submitted some wrong answers for Question 5";
+            ans += getString(R.string.q_five_wrong);
+            score -= 10/2;
         }
+
+
+        // For EditText Field for Question 6
+        if (qSix.getText().toString().equals(getString(R.string.scientist_name))) {
+            score += 10;
+            ans += getString(R.string.q_six_correct);
+        } else {
+            ans += getString(R.string.q_six_wrong);
+        }
+
 
         EditText name = findViewById(R.id.name);
         String name1 = name.getText().toString();
         TextView textView = findViewById(R.id.resultText);
-        textView.setText("\nHi " + name1 + "," + "\n" + ans + getString(R.string.submit_text_view) + score + "/50");
+        textView.setText("\nHi " + name1 + "," + "\n" + ans + getString(R.string.submit_text_view) + score + "/60");
 
         // Toast for showing the result and checking the correct answer
-        if (score != 50) {
+        if (score != 60) {
             Toast.makeText(MainActivity.this, getString(R.string.toast_fail) + score + getString(R.string.toast_fail2), LENGTH_LONG).show();
         } else {
             Toast.makeText(MainActivity.this, getString(R.string.toast_success) + score + getString(R.string.toast_success2), LENGTH_LONG).show();
