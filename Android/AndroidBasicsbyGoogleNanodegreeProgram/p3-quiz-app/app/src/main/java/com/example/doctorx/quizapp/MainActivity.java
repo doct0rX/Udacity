9e078201e09e0c00e36213748fa6a646a6be2417
doct0rX <mustafaj4m@gmail.com>
2018-02-28 13:25:38 +0200

    done checkButtons

--- Android/AndroidBasicsbyGoogleNanodegreeProgram/p3-quiz-app/app/src/main/java/com/example/doctorx/quizapp/MainActivity.java
+++ Android/AndroidBasicsbyGoogleNanodegreeProgram/p3-quiz-app/app/src/main/java/com/example/doctorx/quizapp/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     private int score;
     private RadioButton rb, rb2, rb3, rb4;
+    private CheckBox cb1, cb2;
     private TextView textView;
     private EditText name;
     private String ans;
@@ -60,26 +61,45 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
+        // Check Buttons
+        cb1 = findViewById(R.id.two);
+        cb2 = findViewById(R.id.eleven);
+
+        if (cb1.isChecked()) {
+            score += 10/2;
+        } else {
+            ans += "\n 2 is a Prime Number";
+        }
+        if (cb2.isChecked()) {
+            score += 10/2;
+        } else {
+            ans += "\n 11 is a prime Number";
+        }
+
+        if (cb1.isChecked() && cb2.isChecked()) {
+            ans += "\nYou Answered Question 5 Correctly";
+        }
+        // checkButtons
+//        boolean checked = ((CheckBox) v).isChecked();
+//
+//        // Check which checkbox was clicked
+//        switch (v.getId()) {
+//            case R.id.two:
+//                if (checked) {
+//                    score += 10 / 2;
+//                }
+//                break;
+//            case R.id.seven:
+//                if (checked) {
+//                    score += 10/2;
+//                }
+//                break;
+//        }
+
+
         name = findViewById(R.id.name);
         String name1 = name.getText().toString();
         textView = findViewById(R.id.resultText);
         textView.setText("\nHi " + name1 + "," + "\n" + ans + "\nYour Total Score is: " + score + "/50");
-    }
-
-    // TODO: finish the checkbuttons.
-    public void onCheckboxClicked(View view) {
-        // Is the view now checked?
-        boolean checked = ((CheckBox) view).isChecked();
-
-        // Check which checkbox was clicked
-        switch(view.getId()) {
-            case R.id.two:
-                if (checked)
-                score += 10/3;
-                break;
-            case R.id.seven:
-                if (checked)
-                    score+= 10/3;
-        }
     }
 }
