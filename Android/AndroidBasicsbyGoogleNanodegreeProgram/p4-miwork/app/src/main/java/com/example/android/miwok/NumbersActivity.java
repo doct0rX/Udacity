947ff9d9b0f493dae24dbe477fd4f20178cb7306
doct0rX <mustafaj4m@gmail.com>
2018-03-03 01:33:37 +0200

    added ArrayList to Numbers

--- Android/AndroidBasicsbyGoogleNanodegreeProgram/p4-miwork/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ Android/AndroidBasicsbyGoogleNanodegreeProgram/p4-miwork/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -19,6 +19,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
@@ -27,19 +29,19 @@ public class NumbersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_numbers);
 
          // Adding words array
-        String[] words = new String [10];
-        words[0] = "one";
-        words[1] = "two";
-        words[2] = "three";
-        words[3] = "four";
-        words[4] = "five";
-        words[5] = "six";
-        words[6] = "seven";
-        words[7] = "eight";
-        words[8] = "nine";
-        words[9] = "ten";
+        ArrayList<String> words = new ArrayList<>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
 
-        Log.v("NumberActivity", "Word at index 0: " + words[0]);
-        Log.v("NumberActivity", "Word at index 1: " + words[1]);
+        Log.v("NumberActivity", "Word at index 0: " + words.get(0));
+        Log.v("NumberActivity", "Word at index 1: " + words.get(1));
     }
 }
