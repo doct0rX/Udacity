1448db37343188e8faee5abb7e9e527b719355b0
doct0rX <mustafaj4m@gmail.com>
2018-03-02 22:42:47 +0200

    Array of words

--- Android/AndroidBasicsbyGoogleNanodegreeProgram/p4-miwork/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ Android/AndroidBasicsbyGoogleNanodegreeProgram/p4-miwork/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,6 +17,7 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -24,5 +25,21 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+         // Adding words array
+        String[] words = new String [10];
+        words[0] = "one";
+        words[1] = "two";
+        words[2] = "three";
+        words[3] = "four";
+        words[4] = "five";
+        words[5] = "six";
+        words[6] = "seven";
+        words[7] = "eight";
+        words[8] = "nine";
+        words[9] = "ten";
+
+        Log.v("NumberActivity", "Word at index 0: " + words[0]);
+        Log.v("NumberActivity", "Word at index 1: " + words[1]);
     }
 }
