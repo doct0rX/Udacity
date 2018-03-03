9f76c02447006660d389a2a8cddcdba93ec62cf3
doct0rX <mustafaj4m@gmail.com>
2018-03-03 16:22:54 +0200

    Fixed Bugs Bitches 😁

--- Android/AndroidBasicsbyGoogleNanodegreeProgram/p4-miwork/app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ Android/AndroidBasicsbyGoogleNanodegreeProgram/p4-miwork/app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -3,9 +3,11 @@ package com.example.android.miwok;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -15,9 +17,24 @@ import java.util.ArrayList;
  */
 
 public class WordAdapter extends ArrayAdapter<Word> {
-    @NonNull
+
+    public WordAdapter(Context context, ArrayList<Word> word) {
+        super(context, 0, word);
+    }
+
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
+        Word currentWord = getItem(position);
+        TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwork_text_view);
+        miwokTextView.setText(currentWord.getMiworkTranslation());
+        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+        defaultTextView.setText(currentWord.getDefaultTranslation());
+
+        return listItemView;
     }
 }
