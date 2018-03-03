b789c172bc4d0fc9f7a3d3545da8429ac3d028df
doct0rX <mustafaj4m@gmail.com>
2018-03-03 21:26:26 +0200

    Remaining Words added family added colord added phrases -> now all running <-

--- Android/AndroidBasicsbyGoogleNanodegreeProgram/p4-miwork/app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ Android/AndroidBasicsbyGoogleNanodegreeProgram/p4-miwork/app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -17,12 +17,42 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class FamilyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_family);
+        setContentView(R.layout.word_list);
+
+        // Create a list of family
+        ArrayList<Word> family = new ArrayList<>();
+        family.add(new Word("father", "әpә"));
+        family.add(new Word("mother", "әṭa"));
+        family.add(new Word("son", "angsi"));
+        family.add(new Word("daughter", "tune"));
+        family.add(new Word("older brother", "taachi"));
+        family.add(new Word("younger brother", "chalitti"));
+        family.add(new Word("older sister", "teṭe"));
+        family.add(new Word("younger sister", "kolliti"));
+        family.add(new Word("grandmother", "ama"));
+        family.add(new Word("grandfather", "paapa"));
+
+        // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
+        // adapter knows how to create list items for each item in the list.
+        WordAdapter adapter = new WordAdapter(this, family);
+
+        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        // There should be a {@link ListView} with the view ID called list, which is declared in the
+        // word_list.xml file.
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        // Make the {@link ListView} use the {@link WordAdapter} we created above, so that the
+        // {@link ListView} will display list items for each {@link Word} in the list.
+        assert listView != null;
+        listView.setAdapter(adapter);
     }
 }
--- Android/AndroidBasicsbyGoogleNanodegreeProgram/p4-miwork/app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ Android/AndroidBasicsbyGoogleNanodegreeProgram/p4-miwork/app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -17,12 +17,33 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class PhrasesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_phrases);
+        setContentView(R.layout.word_list);
+
+        // Create a list of Phrases
+        ArrayList<Word> phrases = new ArrayList<>();
+        phrases.add(new Word("Where are you going?", "minto wuksus"));
+        phrases.add(new Word("What is your name?", "tinnә oyaase'nә"));
+        phrases.add(new Word("My name is...", "oyaaset..."));
+        phrases.add(new Word("How are you feeling?", "michәksәs?"));
+        phrases.add(new Word("I’m feeling good.", "kuchi achit"));
+        phrases.add(new Word("Are you coming?", "әәnәs'aa?"));
+        phrases.add(new Word("Yes, I’m coming.", "hәә’ әәnәm"));
+        phrases.add(new Word("I’m coming.", "әәnәm"));
+        phrases.add(new Word("Let’s go.", "yoowutis"));
+        phrases.add(new Word("Come here", "әnni'nem"));
+
+        WordAdapter adapter = new WordAdapter(this, phrases);
+        ListView listView = (ListView) findViewById(R.id.list);
+        assert listView != null;
+        listView.setAdapter(adapter);
     }
 }
