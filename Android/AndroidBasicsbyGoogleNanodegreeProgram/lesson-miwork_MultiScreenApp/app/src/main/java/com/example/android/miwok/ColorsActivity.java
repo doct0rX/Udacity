dd657d7d9b1c86eadec28f1105698355af694828
doct0rX <mustafaj4m@gmail.com>
2018-03-27 13:55:21 +0200

    updated setOnItemClickListener for NumberActivities

--- Android/AndroidBasicsbyGoogleNanodegreeProgram/lesson-miwork_MultiScreenApp/app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ Android/AndroidBasicsbyGoogleNanodegreeProgram/lesson-miwork_MultiScreenApp/app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -30,13 +30,13 @@ public class ColorsActivity extends AppCompatActivity {
 
         // Create a list of colors
         ArrayList<Word> colors = new ArrayList<>();
-        colors.add(new Word("red", "weṭeṭṭi", R.drawable.color_red));
-        colors.add(new Word("green", "chokokki", R.drawable.color_green));
-        colors.add(new Word("brown", "ṭakaakki", R.drawable.color_brown));
-        colors.add(new Word("black", "kululli", R.drawable.color_black));
-        colors.add(new Word("white", "kelelli", R.drawable.color_white));
-        colors.add(new Word("dusty yellow", "ṭopiisә", R.drawable.color_dusty_yellow));
-        colors.add(new Word("mustard yellow", "chiwiiṭә", R.drawable.color_mustard_yellow));
+        colors.add(new Word("red", "weṭeṭṭi", R.drawable.color_red, R.raw.color_red));
+        colors.add(new Word("green", "chokokki", R.drawable.color_green, R.raw.color_green));
+        colors.add(new Word("brown", "ṭakaakki", R.drawable.color_brown, R.raw.color_brown));
+        colors.add(new Word("black", "kululli", R.drawable.color_black, R.raw.color_black));
+        colors.add(new Word("white", "kelelli", R.drawable.color_white, R.raw.color_white));
+        colors.add(new Word("dusty yellow", "ṭopiisә", R.drawable.color_dusty_yellow, R.raw.color_dusty_yellow));
+        colors.add(new Word("mustard yellow", "chiwiiṭә", R.drawable.color_mustard_yellow, R.raw.color_mustard_yellow));
 
         // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
         // adapter knows how to create list items for each item in the list.
--- Android/AndroidBasicsbyGoogleNanodegreeProgram/lesson-miwork_MultiScreenApp/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ Android/AndroidBasicsbyGoogleNanodegreeProgram/lesson-miwork_MultiScreenApp/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -48,7 +48,8 @@ public class NumbersActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                mMediaPlayer = MediaPlayer.create(NumbersActivity.this, R.raw.number_one);
+                Word word = words.get(position);
+                mMediaPlayer = MediaPlayer.create(NumbersActivity.this, word.getAudioResourceID());
                 mMediaPlayer.start();
             }
         });
