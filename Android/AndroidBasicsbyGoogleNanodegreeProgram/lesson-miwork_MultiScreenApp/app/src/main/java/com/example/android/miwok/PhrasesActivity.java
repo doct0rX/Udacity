02459f2c6639487bf7e8a5c769c5b1c2232bb794
doct0rX <mustafaj4m@gmail.com>
2018-03-27 14:13:28 +0200

    setOnItemClickListener for PhrasesActivities

--- Android/AndroidBasicsbyGoogleNanodegreeProgram/lesson-miwork_MultiScreenApp/app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ Android/AndroidBasicsbyGoogleNanodegreeProgram/lesson-miwork_MultiScreenApp/app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -15,13 +15,17 @@
  */
 package com.example.android.miwok;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class PhrasesActivity extends AppCompatActivity {
+    MediaPlayer mMediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,5 +49,14 @@ public class PhrasesActivity extends AppCompatActivity {
         ListView listView = (ListView) findViewById(R.id.list);
         assert listView != null;
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Word word = phrases.get(position);
+                mMediaPlayer = MediaPlayer.create(PhrasesActivity.this, word.getAudioResourceID());
+                mMediaPlayer.start();
+            }
+        });
     }
 }
