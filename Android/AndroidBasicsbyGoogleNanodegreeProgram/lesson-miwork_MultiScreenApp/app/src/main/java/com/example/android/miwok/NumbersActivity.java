edd9468bb9e74567e8191ba3cd56685479c09b76
doct0rX <mustafaj4m@gmail.com>
2018-03-28 20:02:05 +0200

    correctly position 'releaseMediaPlayer() methodd' into the code

--- Android/AndroidBasicsbyGoogleNanodegreeProgram/lesson-miwork_MultiScreenApp/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ Android/AndroidBasicsbyGoogleNanodegreeProgram/lesson-miwork_MultiScreenApp/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -12,6 +12,16 @@ import java.util.ArrayList;
 public class NumbersActivity extends AppCompatActivity {
     private MediaPlayer mMediaPlayer;
 
+    /**
+     * This listener gets triggered when the {@link MediaPlayer} has completed playing the audio file.
+     */
+    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mp) {
+            releaseMediaPlayer();
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,27 +61,33 @@ public class NumbersActivity extends AppCompatActivity {
                 // Get the {@link Word} object at the given position the user clicked on
                 Word word = words.get(position);
 
+                // Release the media player if it currently exists because we are about to play a different sound file
+                releaseMediaPlayer();
+
                 // Create and setup the {@link MediaPlayer} for the audio resource associated w/ the current word
                 mMediaPlayer = MediaPlayer.create(NumbersActivity.this, word.getAudioResourceID());
                 mMediaPlayer.start();
+
+                // Setup a completion on the media player, so that we can stop and release the media player once the sounds has finished playing.
+                mMediaPlayer.setOnCompletionListener(mCompletionListener);
             }
         });
+    }
 
-        /**
-         * Clean up the media player by releasing its resources.
-         */
-        private void releaseMediaPlayer() {
-            // If the media player is not null, then it may be currently playing a sound.
-            if (mMediaPlayer != null) {
-                // Regardless of the current state of the media player, release its resources
-                // because we no longer need it.
-                mMediaPlayer.release();
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mMediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mMediaPlayer.release();
 
-                // Set the media player back to null. For our code, we've decided that
-                // setting the media player to null is an easy way to tell that the media player
-                // is not configured to play an audio file at the moment.
-                mMediaPlayer = null;
-            }
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mMediaPlayer = null;
         }
     }
 }
