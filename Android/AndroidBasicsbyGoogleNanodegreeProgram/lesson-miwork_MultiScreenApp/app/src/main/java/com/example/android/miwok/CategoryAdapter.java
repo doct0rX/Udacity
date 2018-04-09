1ca5b1b44d2b0b8c97682ff6f0c14e66057e75ea
doct0rX <mustafaj4m@gmail.com>
2018-04-09 16:48:03 +0200

    ViewPager and FragmentPagerAdapter

---
+++ Android/AndroidBasicsbyGoogleNanodegreeProgram/lesson-miwork_MultiScreenApp/app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -0,0 +1,41 @@
+package com.example.android.miwok;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by doctorX on 4/9/18.
+ *
+ * {@link CategoryAdapter} is a {@link FragmentPagerAdapter} that can provide the layout for
+ * each list item based on a data source which is a list of {@link Word} objects.
+ */
+public class CategoryAdapter extends FragmentPagerAdapter {
+    public CategoryAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    /**
+     * Return the {@link Fragment} that should be displayed for the given page number.
+     */
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new NumbersFragment();
+        } else if (position == 1) {
+            return new FamilyFragment();
+        } else if (position == 2) {
+            return new ColorsFragment();
+        } else {
+            return new PhrasesFragment();
+        }
+    }
+
+    /**
+     * Return the total number of pages.
+     */
+    @Override
+    public int getCount() {
+        return 4;
+    }
+}
