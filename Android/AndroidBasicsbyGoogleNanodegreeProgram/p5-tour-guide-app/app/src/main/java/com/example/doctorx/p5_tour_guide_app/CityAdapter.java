4137e872136f60dafb6d289983c8eab5b685f123
doct0rX <mustafaj4m@gmail.com>
2018-05-01 21:47:04 +0200

    City Activity * CityAdapter custom class * grid_single * activity_main * MainActivity

---
+++ Android/AndroidBasicsbyGoogleNanodegreeProgram/p5-tour-guide-app/app/src/main/java/com/example/doctorx/p5_tour_guide_app/CityAdapter.java
@@ -0,0 +1,64 @@
+package com.example.doctorx.p5_tour_guide_app;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.widget.BaseAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+/**
+ * Created by doctorX on 4/20/18.
+ *
+ */
+
+public class CityAdapter extends BaseAdapter {
+    private Context mContext;
+    private final String[] cityName;
+    private final int[] imageId;
+
+    public CityAdapter(Context c, String[] cityName, int[] imageId) {
+        mContext = c;
+        this.imageId = imageId;
+        this.cityName = cityName;
+    }
+
+    @Override
+    public int getCount() {
+        return cityName.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        // TODO: Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View grid;
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+	        if (convertView == null) {
+
+	        	grid = new View(mContext);
+				grid = inflater.inflate(R.layout.grid_single, null);
+	        	TextView textView =  grid.findViewById(R.id.grid_text);
+	        	ImageView imageView = grid.findViewById(R.id.grid_image);
+	        	textView.setText(cityName[position]);
+	        	imageView.setImageResource(imageId[position]);
+	        } else {
+	        	grid = convertView;
+	        }
+
+        return grid;
+    }
+}
--- Android/AndroidBasicsbyGoogleNanodegreeProgram/p5-tour-guide-app/app/src/main/java/com/example/doctorx/p5_tour_guide_app/MainActivity.java
+++ Android/AndroidBasicsbyGoogleNanodegreeProgram/p5-tour-guide-app/app/src/main/java/com/example/doctorx/p5_tour_guide_app/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.doctorx.p5_tour_guide_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    GridView gridView;
+    String[] cityName = {"Alex", "Cairo", "Luxor", "Aswan"};
+    int[] imageId = {R.drawable.alex, R.drawable.cairo, R.drawable.luxor, R.drawable.aswan};
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CityAdapter adapter = new CityAdapter(MainActivity.this, cityName, imageId);
+        gridView = findViewById(R.id.grid);
+        gridView.setAdapter(adapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "You Clicked at " +cityName[+ position], Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
