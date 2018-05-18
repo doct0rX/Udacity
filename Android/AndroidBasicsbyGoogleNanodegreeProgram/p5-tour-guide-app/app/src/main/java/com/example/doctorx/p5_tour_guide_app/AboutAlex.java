670557067ebe0b59461b74b5a9940b60f68a4f4b
doct0rX <mustafaj4m@gmail.com>
2018-05-18 14:03:20 +0200

    2 updates removed extra activities updateed AboutAlex to display Alex location

--- Android/AndroidBasicsbyGoogleNanodegreeProgram/p5-tour-guide-app/app/src/main/java/com/example/doctorx/p5_tour_guide_app/AboutAlex.java
+++ Android/AndroidBasicsbyGoogleNanodegreeProgram/p5-tour-guide-app/app/src/main/java/com/example/doctorx/p5_tour_guide_app/AboutAlex.java
@@ -1,14 +1,30 @@
 package com.example.doctorx.p5_tour_guide_app;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class AboutAlex extends AppCompatActivity {
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.container);
-        getSupportFragmentManager().beginTransaction().replace(R.id.container, new AboutFragment()).commit();
+        setContentView(R.layout.about_alex);
+
+        Button btn = findViewById(R.id.map);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri gmmIntentUri = Uri.parse("geo:38.8048,-77.0469");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                if (mapIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(mapIntent);
+                }
+            }
+        });
     }
 }
