8c26d19b2c7c59d000421b647b94865d3cc924e4
doct0rX <mustafaj4m@gmail.com>
2018-06-26 22:54:19 +0200

    Handle Empty and null cases

--- Android/AndroidBasicsbyGoogleNanodegreeProgram/lessons/DidYouFeelItApp/app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
+++ Android/AndroidBasicsbyGoogleNanodegreeProgram/lessons/DidYouFeelItApp/app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
@@ -57,17 +57,45 @@ public class MainActivity extends AppCompatActivity {
         magnitudeTextView.setText(earthquake.perceivedStrength);
     }
 
+    /**
+     * {@link AsyncTask} to perform the network request on a background thread, and then
+     * update the UI with the first earthquake in the response.
+     */
     private class EarthquakeAsyncTask extends AsyncTask<String, Void, Event> {
 
+        /**
+         * This method is invoked (or called) on a background thread, so we can perform
+         * long-running operations like making a network request.
+         *
+         * It is NOT okay to update the UI from a background thread, so we just return an
+         * {@link Event} object as the result.
+         */
         @Override
         protected Event doInBackground(String... urls) {
+            // Don't perform the request if there are no URLs, or the first URL is null.
+            if (urls.length < 1 || urls[0] == null) {
+                return null;
+            }
+
             // Perform the HTTP request for earthquake data and process the response.
             Event result = Utils.fetchEarthquakeData(urls[0]);
             return result;
         }
 
+        /**
+         * This method is invoked on the main UI thread after the background work has been
+         * completed.
+         *
+         * It IS okay to modify the UI within this method. We take the {@link Event} object
+         * (which was returned from the doInBackground() method) and update the views on the screen.
+         */
         @Override
         protected void onPostExecute(Event result) {
+            // If there is no result, do nothing.
+            if (result == null) {
+                return;
+            }
+
             updateUi(result);
         }
     }
