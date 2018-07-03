88c5ff0020b4f9e1ffffe832a9b3f13ab984a09a
doct0rX <mustafaj4m@gmail.com>
2018-07-03 22:39:37 +0200

    till manifest file

---
+++ Android/AndroidBasicsbyGoogleNanodegreeProgram/lessons/QuakeReportApp/app/src/main/java/com/example/android/quakereport/SettingsActivity.java
@@ -0,0 +1,18 @@
+package com.example.android.quakereport;
+
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+    }
+
+    public static class EarthquakePreferenceFragment extends PreferenceFragment {
+
+    }
+}
