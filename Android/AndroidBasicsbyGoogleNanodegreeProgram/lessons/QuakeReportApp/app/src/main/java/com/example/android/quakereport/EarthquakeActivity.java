1257662d764628299caa368768df1b4563f30579
doct0rX <mustafaj4m@gmail.com>
2018-07-03 23:28:34 +0200

    added menu to mainActivity

--- Android/AndroidBasicsbyGoogleNanodegreeProgram/lessons/QuakeReportApp/app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ Android/AndroidBasicsbyGoogleNanodegreeProgram/lessons/QuakeReportApp/app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -25,6 +25,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -156,5 +158,26 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
 
         // Loader reset, so we can clear out our existing data.
         mAdapter.clear();
+    }
+
+
+    // For Menu;
+    @Override
+    // This method initialize the contents of the Activity's options menu.
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the Options Menu we specified in XML
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
