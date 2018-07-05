d7b51eb6bf779879bd1de4d451eda2efcc1d307d
doct0rX <mustafaj4m@gmail.com>
2018-07-05 15:58:31 +0200

    Order by Preference the End

--- Android/AndroidBasicsbyGoogleNanodegreeProgram/lessons/QuakeReportApp/app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ Android/AndroidBasicsbyGoogleNanodegreeProgram/lessons/QuakeReportApp/app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -140,6 +140,10 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
                 getString(R.string.settings_min_magnitude_key),
                 getString(R.string.settings_min_magnitude_default));
 
+        String orderBy  = sharedPrefs.getString(
+                getString(R.string.settings_order_by_key),
+                getString(R.string.settings_order_by_default));
+
         // parse breaks apart the URI string that's passed into its parameter
         Uri baseUri = Uri.parse(USGS_REQUEST_URL);
 
@@ -150,7 +154,7 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
         uriBuilder.appendQueryParameter("format", "geojson");
         uriBuilder.appendQueryParameter("limit", "10");
         uriBuilder.appendQueryParameter("minmag", minMagnitude);
-        uriBuilder.appendQueryParameter("orderby", "time");
+        uriBuilder.appendQueryParameter("orderby", orderBy);
 
         // Return the completed uri `http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&limit=10&minmag=minMagnitude&orderby=time
         return new EarthquakeLoader(this, uriBuilder.toString());
--- Android/AndroidBasicsbyGoogleNanodegreeProgram/lessons/QuakeReportApp/app/src/main/java/com/example/android/quakereport/SettingsActivity.java
+++ Android/AndroidBasicsbyGoogleNanodegreeProgram/lessons/QuakeReportApp/app/src/main/java/com/example/android/quakereport/SettingsActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.quakereport;
 
 import android.content.SharedPreferences;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
@@ -24,13 +25,26 @@ public class SettingsActivity extends AppCompatActivity {
 
             Preference minMagnitude = findPreference(getString(R.string.settings_min_magnitude_key));
             bindPreferenceSummaryToValue(minMagnitude);
+
+            Preference orderBy = findPreference(getString(R.string.settings_order_by_key));
+            bindPreferenceSummaryToValue(orderBy);
         }
 
         @Override
         public boolean onPreferenceChange(Preference preference, Object newValue) {
             // The code in this method takes care of updating the displayed preference summary after it has been changed
             String stringValue = newValue.toString();
-            preference.setSummary(stringValue);
+            if (preference instanceof ListPreference) {
+                ListPreference listPreference = (ListPreference) preference;
+                int prefIndex = listPreference.findIndexOfValue(stringValue);
+                if (prefIndex >= 0) {
+                    CharSequence[] labels = listPreference.getEntries();
+                    preference.setSummary(labels[prefIndex]);
+                }
+            } else {
+                preference.setSummary(stringValue);
+            }
+
             return true;
         }
 
