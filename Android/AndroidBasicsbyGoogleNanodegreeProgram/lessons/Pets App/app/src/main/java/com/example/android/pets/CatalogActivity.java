c594690be300b50a3bf9226102574eb8aff7efa9
doct0rX <mustafaj4m@gmail.com>
2018-07-19 19:40:47 +0200

    handling a cursor object

--- Android/AndroidBasicsbyGoogleNanodegreeProgram/lessons/Pets App/app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ Android/AndroidBasicsbyGoogleNanodegreeProgram/lessons/Pets App/app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -77,9 +77,24 @@ public class CatalogActivity extends AppCompatActivity {
         // Create and/or open a database to read from it
         SQLiteDatabase db = mDbHelper.getReadableDatabase();
 
-        // Perform this raw SQL query "SELECT * FROM pets"
-        // to get a Cursor that contains all rows from the pets table.
-        Cursor cursor = db.rawQuery("SELECT * FROM " + PetEntry.TABLE_NAME, null);
+        // defining my projection
+        String[] projection = {
+                PetEntry._ID,
+                PetEntry.COLUMN_PET_NAME,
+                PetEntry.COLUMN_PET_BREED,
+                PetEntry.COLUMN_PET_GENDER,
+                PetEntry.COLUMN_PET_WEIGHT
+        };
+
+        Cursor cursor = db.query(
+                PetEntry.TABLE_NAME,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                null);
+        
         try {
             // Display the number of rows in the Cursor (which reflects the number of rows in the
             // pets table in the database).
