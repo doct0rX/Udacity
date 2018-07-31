a551f98bbb2867b759faad4f8367ac3760788f0d
doct0rX <mustafaj4m@gmail.com>
2018-07-31 21:42:14 +0200

    p8 stage2 upon p8 stage1

---
+++ Android/AndroidBasicsbyGoogleNanodegreeProgram/p8-inventory-app-stage2/app/src/main/java/com/example/doctorx/inventoryapp/EditorActivity.java
@@ -0,0 +1,120 @@
+package com.example.doctorx.inventoryapp;
+
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.opengl.Visibility;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.doctorx.inventoryapp.data.ProductContract.ProductEntry;
+import com.example.doctorx.inventoryapp.data.ProductDbHelper;
+
+public class EditorActivity extends AppCompatActivity {
+
+    private EditText mNameEditText;
+    private EditText mPriceEditText;
+    private EditText mQuantityEditText;
+    private EditText mSupplierEditText;
+    private EditText mPhoneEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+
+        // Find all relevant views that we will need to read user input from
+        mNameEditText = findViewById(R.id.edit_product_name);
+        mPriceEditText = findViewById(R.id.edit_product_price);
+        mQuantityEditText = findViewById(R.id.edit_product_quantity);
+        mSupplierEditText = findViewById(R.id.edit_product_supplier);
+        mPhoneEditText = findViewById(R.id.edit_product_phone);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_editor.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
+    }
+
+    private void insertProduct(){
+
+        String nameString = mNameEditText.getText().toString().trim();
+        String priceString = mPriceEditText.getText().toString().trim();
+        int price = 0;
+        if (!priceString.isEmpty()) {
+            price = Integer.parseInt(priceString);
+        } else {
+            Toast.makeText(this, "Error saving the Price with dummy data: 0", Toast.LENGTH_SHORT).show();
+        }
+        String quantityString = mQuantityEditText.getText().toString().trim();
+        int quantity = 0;
+        if (!quantityString.isEmpty()) {
+            quantity = Integer.parseInt(quantityString);
+        } else {
+            Toast.makeText(this, "Error saving the Quantity with dummy data: 0", Toast.LENGTH_SHORT).show();
+        }
+        String supplierString = mSupplierEditText.getText().toString().trim();
+        String phoneString = mPhoneEditText.getText().toString().trim();
+        int phone = 0;
+        if (!phoneString.isEmpty()) {
+            phone = Integer.parseInt(phoneString);
+        } else {
+            Toast.makeText(this, "Error saving the Phone Number with dummy data: 0", Toast.LENGTH_SHORT).show();
+        }
+
+        // Create database helper
+        ProductDbHelper mDbHelper = new ProductDbHelper(EditorActivity.this);
+
+        // Gets the database in write mode
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        // Create a ContentValues object where column names are the keys,
+        // and Toto's pet attributes are the values.
+        ContentValues values = new ContentValues();
+        values.put(ProductEntry.COLUMN_PRODUCT_NAME, nameString);
+        values.put(ProductEntry.COLUMN_PRODUCT_PRICE, price);
+        values.put(ProductEntry.COLUMN_PRODUCT_QUANTITY, quantity);
+        values.put(ProductEntry.COLUMN_PRODUCT_SUPPLIER, supplierString);
+        values.put(ProductEntry.COLUMN_PRODUCT_PHONE, phone);
+
+        // Insert a new row for pet in the database, returning the ID of that new row.
+        long newRowId = db.insert(ProductEntry.TABLE_NAME, null, values);
+
+        // Show a toast message depending on whether or not the insertion was successful
+        if (newRowId == -1) {
+            // If the row ID is -1, then there was an error with insertion.
+            Toast.makeText(this, "Error with saving product", Toast.LENGTH_SHORT).show();
+        } else {
+            // Otherwise, the insertion was successful and we can display a toast with the row ID.
+            Toast.makeText(this, "Product saved with row id: " + newRowId, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Save" menu option
+            case R.id.action_save:
+                // Add pet
+                insertProduct();
+                // Exit activity
+                finish();
+                return true;
+            // Respond to a click on the "Up" arrow button in the app bar
+            case android.R.id.home:
+                // Navigate back to parent activity (CatalogActivity)
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ Android/AndroidBasicsbyGoogleNanodegreeProgram/p8-inventory-app-stage2/app/src/main/java/com/example/doctorx/inventoryapp/ProductMain.java
@@ -0,0 +1,152 @@
+package com.example.doctorx.inventoryapp;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.example.doctorx.inventoryapp.data.ProductContract.ProductEntry;
+import com.example.doctorx.inventoryapp.data.ProductDbHelper;
+
+public class ProductMain extends AppCompatActivity {
+
+    /** Database helper that will provide us access to the database */
+    private ProductDbHelper mDbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_main);
+
+        // Setup FAB to open EditorActivity
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ProductMain.this, EditorActivity.class));
+            }
+        });
+
+        mDbHelper = new ProductDbHelper(this);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        displayDatabaseInfo();
+    }
+
+    private void displayDatabaseInfo() {
+
+        // Create and/or open a database to read from it
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+
+        String[] projection = {
+                ProductEntry._ID,
+                ProductEntry.COLUMN_PRODUCT_NAME,
+                ProductEntry.COLUMN_PRODUCT_PRICE,
+                ProductEntry.COLUMN_PRODUCT_QUANTITY,
+                ProductEntry.COLUMN_PRODUCT_SUPPLIER,
+                ProductEntry.COLUMN_PRODUCT_PHONE
+        };
+
+        Cursor cursor = db.query(
+                ProductEntry.TABLE_NAME,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                null);
+
+        TextView displayView = findViewById(R.id.text_view_product);
+
+        try {
+            // In the while loop below, iterate through the rows of the cursor and display
+            // the information from each column in this order.
+            displayView.setText("The products table contains " + cursor.getCount() + " products.\n\n");
+            displayView.append(ProductEntry._ID + " - " +
+                    ProductEntry.COLUMN_PRODUCT_NAME + " - " +
+                    ProductEntry.COLUMN_PRODUCT_PRICE + " - " +
+                    ProductEntry.COLUMN_PRODUCT_QUANTITY + " - " +
+                    ProductEntry.COLUMN_PRODUCT_SUPPLIER + " - " +
+                    ProductEntry.COLUMN_PRODUCT_PHONE + "\n");
+
+            // Figure out the index of each column
+            int idColumnIndex = cursor.getColumnIndex(ProductEntry._ID);
+            int nameColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_NAME);
+            int priceColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_PRICE);
+            int quantityColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_QUANTITY);
+            int supplierColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_SUPPLIER);
+            int phoneColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_PHONE);
+
+            // Iterate through all the returned rows in the cursor
+            while (cursor.moveToNext()) {
+                // Use that index to extract the String or Int value of the word
+                // at the current row the cursor is on.
+                int currentID = cursor.getInt(idColumnIndex);
+                String currentName = cursor.getString(nameColumnIndex);
+                String currentPrice = cursor.getString(priceColumnIndex);
+                String currentQuantity = cursor.getString(quantityColumnIndex);
+                String currentSupplier = cursor.getString(supplierColumnIndex);
+                String currentPhone = cursor.getString(phoneColumnIndex);
+                // Display the values from each column of the current row in the cursor in the TextView
+                displayView.append(("\n" + currentID + " - " +
+                        currentName + " - "
+                        + currentPrice + " - "
+                        + currentQuantity + " - "
+                        + currentSupplier + " - "
+                        + currentPhone));
+            }
+
+        } finally {
+            // Closes the cursor
+            cursor.close();
+        }
+    }
+
+    private void insertProduct() {
+        // Gets the database in write mode
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        // Create a ContentValues object where column names are the keys,
+        // and Example's product attributes are the values.
+        ContentValues values = new ContentValues();
+        values.put(ProductEntry.COLUMN_PRODUCT_NAME, "Headphones");
+        values.put(ProductEntry.COLUMN_PRODUCT_PRICE, 120);
+        values.put(ProductEntry.COLUMN_PRODUCT_QUANTITY, 5);
+        values.put(ProductEntry.COLUMN_PRODUCT_SUPPLIER, "Sol Republic");
+        values.put(ProductEntry.COLUMN_PRODUCT_PHONE, 555222111);
+
+        db.insert(ProductEntry.TABLE_NAME, null, values);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_products.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_product_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Insert dummy data" menu option
+            case R.id.action_insert_dummy_data:
+                // Insert dummy data
+                insertProduct();
+                displayDatabaseInfo();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
